35152ee629354bdf6f4f73bf7af5e3ff0b3d9089
Dustin <demonicpagan@gmail.com>
2015-01-05 08:22:38 -0500

    Hopefully adding display of StormbotTCL
    Signed-off-by: Dustin <demonicpagan@gmail.com>

--- assets/js/app.js
+++ assets/js/app.js
@@ -38,5 +38,12 @@
 		include_github_logo: false
 	});
 
+	$("#github_user_badge2").GitHubBadge({
+		login: "stormbottcl",
+		sort_on: "date", // "date" or "name"
+		sorting: "descending",
+		include_github_logo: false
+	});
+
 	$('#twitter-widget-0').css('width','100%')
 })(jQuery, this);
