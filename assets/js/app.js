2c0d55a5b1f031dbf5d512e2d3898a7e128bb1ac
Dustin <demonicpagan@gmail.com>
2015-01-14 08:02:47 -0500

    Removed modernizer js
    Signed-off-by: Dustin <demonicpagan@gmail.com>

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,18 +1,6 @@
 ;(function ($, window, undefined) {
 	'use strict';
 
-	var $doc = $(document),
-		Modernizr = window.Modernizr;
-
-	// Hide address bar on mobile devices
-	if (Modernizr.touch) {
-		$(window).load(function () {
-			setTimeout(function () {
-				window.scrollTo(0, 1);
-			}, 0);
-		});
-	}
-
 	$("#github_user_badge").GitHubBadge({
 		login: "demonicpagan",
 		sort_on: "date", // "date" or "name"
